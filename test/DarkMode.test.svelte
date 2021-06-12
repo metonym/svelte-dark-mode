c31805fc9dbd248eb9395837da6d8a46b218c87a
Eric Liu <ericyl.us@gmail.com>
2021-06-12 04:40:15 -0700

    chore: create test folder

--- test/DarkMode.test.svelte
+++ test/DarkMode.test.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import DarkMode from "./types";
-  import { Theme } from "./types/DarkMode";
+  import DarkMode from "../types";
+  import { Theme } from "../types/DarkMode";
 
   let theme: Theme = "light";
   let switchTheme: Theme;
@@ -9,19 +9,13 @@
   $: document.body.className = theme; // "dark" or "light"
 </script>
 
-<style>
-  :global(.dark) {
-    background: #032f62;
-    color: #f1f8ff;
-  }
-</style>
-
 <DarkMode
   key="key"
   bind:theme
   on:change={(e) => {
     console.log(e.detail);
-  }} />
+  }}
+/>
 
 <h1>This is {theme} mode.</h1>
 <p>Change the theme and reload the page. The theme should be persisted.</p>
@@ -30,8 +24,16 @@
   type="button"
   on:click={() => {
     theme = switchTheme;
-  }}>
+  }}
+>
   Switch to
   {switchTheme}
   mode
 </button>
+
+<style>
+  :global(.dark) {
+    background: #032f62;
+    color: #f1f8ff;
+  }
+</style>
