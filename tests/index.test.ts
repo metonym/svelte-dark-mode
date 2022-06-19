a0dd784b09c20247730ae0863a8ccb48c41df1d6
Eric Liu <ericyl.us@gmail.com>
2022-06-19 10:54:47 -0700

    test: add unit testing using vitest

---
+++ tests/index.test.ts
@@ -0,0 +1,10 @@
+import { test, expect } from "vitest";
+import * as API from "../src";
+
+test("Library has exports", () => {
+  expect(Object.keys(API)).toMatchInlineSnapshot(`
+    [
+      "default",
+    ]
+  `);
+});
--- vitest.config.js
+++ vitest.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import preprocess from "svelte-preprocess";
 
 export default defineConfig({
   plugins: [
     svelte({
       hot: !process.env.VITEST,
+      preprocess: [preprocess()]
     }),
   ],
   test: {
